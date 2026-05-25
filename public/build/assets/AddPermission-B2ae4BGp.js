748b29f1ed474c7a9543110be91565db4c0d52a4
Mudasir Abbas Turi <mudasirabbas577@gmail.com>
2026-05-25 18:07:03 +0500

    rebuild

---
+++ public/build/assets/AddPermission-B2ae4BGp.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Crm7f7tY.js";const s=()=>r.jsx(r.Fragment,{children:r.jsx("h1",{children:"Add Permission Will Go Here:"})});export{s as default};
--- public/build/assets/Home-DweYdMiu.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DI20nL_T.js";const t=()=>r.jsx(r.Fragment,{children:r.jsx("h1",{children:"HOME PAGE."})});export{t as default};
